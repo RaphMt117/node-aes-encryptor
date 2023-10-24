c38d330505648de0d2832b755f138e2c39a4d431
RaphMt <rafa.mtorres117@outlook.com>
2023-10-23 23:45:26 -0300

    commit test

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const scanner = require('prompt-sync')();
 const data = scanner('Insira o dado a ser encriptado: ');
 var dataBytes = aesjs.utils.utf8.toBytes(data)
 
-// To print or store the binary data, you may convert it to hex
+// Para exibir o dado binário armazenado, ele deve ser passado para Hex
 var dataHex = aesjs.utils.hex.fromBytes(dataBytes);
 
 console.log('Dado encriptado: ' + dataHex);
+
